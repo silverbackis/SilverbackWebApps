4f5034263e21426e6919695c7c2848d82dae0fd8
Daniel West <daniel@silverback.is>
2019-03-08 22:05:21 +0000

    Hero blue background teal circle mask/clip

--- app/components/hero/Home.vue
+++ app/components/hero/Home.vue
@@ -6,6 +6,11 @@
            xmlns="http://www.w3.org/2000/svg"
            xmlns:xlink="http://www.w3.org/1999/xlink"
            viewBox="-2 -2 760 1087" xml:space="preserve">
+        <defs>
+          <mask id="mask">
+            <circle cx="50%" cy="50%" r="100%" fill="white" ref="circleMask" />
+          </mask>
+        </defs>
         <path ref="svgLogo__noFace" class="path noface" d="M322.2,0C129.2,0,21.8,90.8,0,272.3c2.5,6.7,3.6,83.8,3.6,83.8l3.3,30.1l6.3,39.3l4.3,20.2l7.9,30l10.3,30.6
 	l15.4,35.2l12.8,23l14.7,21.5l18.2,21.4l17.2,16l10.4,8.2l10.8,7.8c44.6,23,100,34.5,166,34.5h147.5c19.7,0,34.2,5.8,43.4,17.3
 	c9.2,11.5,13.9,23.8,13.9,37.1s-4.6,25.6-13.9,37.1s-23.7,17.2-43.4,17.2H150.3c-90.2,0-136.6,50.1-138.8,150.3l-0.2,150h125.9
@@ -20,7 +25,7 @@
           c0,13.3-4.6,25.6-13.9,37.1c-9.3,11.5-23.7,17.2-43.4,17.2H150.3c-90.2,0-136.6,50.1-138.8,150.3l-0.2,150h125.9
           c3.7,0.2,7.4,0.4,11.3,0.4h284.6C648.4,1083,756,967.7,756,737.1c0-218.5-101.8-327.8-305.4-327.8l-147.6,0
           c-19.7,0-34.1-5.7-43.3-17.2c-9.3-11.5-14-23.8-14-37.1c0-13.3,4.6-25.6,14-37.1c9.2-11.5,23.7-17.2,43.3-17.2h246.4
-          c90.2,0,136.5-50.1,138.8-150.3v-5.4c0-0.3,0-48.6,0-144.7H554.7c-1.2,0-2.3-0.1-3.4-0.1H322.2" />
+          c90.2,0,136.5-50.1,138.8-150.3v-5.4c0-0.3,0-48.6,0-144.7H554.7c-1.2,0-2.3-0.1-3.4-0.1H322.2" mask="url(#mask)" />
 
         <path ref="svgLogo__top" class="path logo top" d="M322.2,0C129.2,0,21.8,90.8,0,272.3c11.9,5.1,90.2,39.2,109.3,62.9c21.5-1.2,35.5,13.4,39.8,21.2
           c8.2,1.6,7,9.8,7,9.8s-3.5,22.4-10.2,28.5c4.3,7.3-1.6,10.6-1.6,10.6s-2.7,13,13.3,26.1c15.6,7.3,25.8,4.1,44.5,27.7
@@ -47,12 +52,12 @@ export default {
     const chars = heroSplitText.chars
     this.$gsap.tween.set(".hero .title", {perspective:400});
 
-    const timeline = new this.$gsap.timeline()
+    const timeline = new this.$gsap.timeline({ repeat: -1, repeatDelay: 2 })
     timeline
       .set(this.$refs.svgLogo__top, { stroke: 'white' })
       .fromTo(
         this.$refs.svgLogo__top,
-        1,
+      .8,
         {
           drawSVG: "0% 0%"
         },
@@ -74,7 +79,6 @@ export default {
         },
         "line2"
       )
-      .fromTo(['.hero', this.$refs.svgLogo__noFace], .4, { background: '#4ba2a1' }, { background: 'rgb(33, 46, 99)', stroke: 'rgba(33, 46, 99, 0)' }, "line2+=.4")
       .from(
         [this.$refs.svgLogo__top, this.$refs.svgLogo__back],
         3,
@@ -84,6 +88,7 @@ export default {
         },
         "-=.3"
       )
+      .from(this.$refs.circleMask, 2, { scale: 0, transformOrigin: '50% 50%' }, "line2-=.3")
       .staggerFrom(
         chars,
         0.7,
@@ -96,15 +101,6 @@ export default {
           ease: Back.easeOut
         },
         0.04,
-        "line2+=.5"
-      )
-      .from(
-        '.navbar-item.is-logo',
-        .5,
-        {
-          x: -50,
-          opacity: 0
-        },
         "line2+=.5"
       )
   }
@@ -121,10 +117,10 @@ export default {
     &.back
       stroke: transparent
     &.top
+      fill: transparent
       stroke: $teal
     &.noface
-      fill: transparent
-      stroke: transparent
+      display: none
   .home-hero
     .hero-svg
       position: absolute
